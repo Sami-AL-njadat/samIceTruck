42a91869c334be101c8b50183c5797551b2037eb
Sami AL-Najadat <sami.alnajadat@gmail.com>
2025-03-14 03:33:32 +0300

    sami2222

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -434,17 +434,24 @@
 
 
 
-    <!-- copyright section end -->
-    <!-- Javascript files-->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('js/jquery-3.0.0.min.js') }}"></script>
-    <script src="{{ asset('js/plugin.js') }}"></script>
-    <!-- sidebar -->
-    <script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
-    <!-- javascript -->
+ 
+
+
+
+
+
+
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/plugin.js') }}"></script>
+<script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+
+
+
+
 
     <script>
         var preloader = document.getElementById("preloader");
