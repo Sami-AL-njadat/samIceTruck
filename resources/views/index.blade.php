fa52149b4f0a8f4539b664077c18a08b2f124ad1
Sami AL-Najadat <sami.alnajadat@gmail.com>
2025-03-14 03:50:52 +0300

    sami222111qqq2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
+    
     <!-- bootstrap css -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <!-- style css -->
@@ -327,7 +328,7 @@
                                             voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
                                         </p>
                                         <h4 class="client_name">Marri Fen</h4>
-                                        <div class="client_img"><img src="images/client-img.png" loading="lazy">
+                                        <div class="client_img"><img src="{{asset('images/client-img.png')}}" >
                                         </div>
                                     </div>
                                     <div class="carousel-item">
@@ -337,7 +338,7 @@
                                             voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
                                         </p>
                                         <h4 class="client_name">Marri Fen</h4>
-                                        <div class="client_img"><img src="images/client-img.png" loading="lazy">
+                                        <div class="client_img"><img src="{{asset('images/client-img.png')}}" >
                                         </div>
                                     </div>
                                     <div class="carousel-item">
@@ -347,7 +348,7 @@
                                             voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
                                         </p>
                                         <h4 class="client_name">Marri Fen</h4>
-                                        <div class="client_img"><img src="images/client-img.png" loading="lazy">
+                                        <div class="client_img"><img src="{{asset('images/client-img.png')}}" >
                                         </div>
                                     </div>
                                 </div>
@@ -434,32 +435,26 @@
 
 
 
- 
+    <!-- copyright section end -->
+    <!-- Javascript files-->
+    {{-- <script src="{{ asset('js/jquery.min.js') }}"></script> --}}
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.0.0.min.js') }}"></script>
+    <script src="{{ asset('js/plugin.js') }}"></script>
+    <!-- sidebar -->
+    <script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+    <!-- javascript -->
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
 
 
 
-
-
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/popper.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('js/plugin.js') }}"></script>
-<script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-<script src="{{ asset('js/custom.js') }}"></script>
-
-
-
-<script>
-    $(document).ready(function(){
-        $('#main_slider').carousel({
-            interval: 3000,  // لتحديد وقت الانتقال بين الشرائح (بالميلي ثانية)
-            pause: 'hover'   // يتوقف التمرير عند وضع المؤشر على الكاروسيل
-        });
-    });
-</script>
-
+    
 
     <script>
         var preloader = document.getElementById("preloader");
