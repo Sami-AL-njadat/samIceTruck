141ae6d9756e5ba58d3abb3d413f44b4f4dc1187
Sami AL-Najadat <sami.alnajadat@gmail.com>
2025-03-15 23:23:39 +0300

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -445,10 +445,12 @@
     <!-- copyright section end -->
     <!-- Javascript files-->
  
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script src="{{asset('js/jquery.min.js')}}"></script>
  
     <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+          <script src="{{asset('js/jquery-3.0.0.min.js')}}"></script>
+
     <script src="{{ asset('js/plugin.js') }}"></script>
     <!-- sidebar -->
     <script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
