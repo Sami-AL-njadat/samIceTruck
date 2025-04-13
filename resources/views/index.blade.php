d820a73bd0ad934f21338e24aeba2ee834cb4cf9
Sami AL-Najadat <sami.alnajadat@gmail.com>
2025-04-13 21:06:57 +0300

    sami is bestqqqssaaa qششqششaaaaaa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -456,7 +456,7 @@
                                         <div class="client_img">
                                             <img src="{{ asset('images/WheatonChicagos.png') }}"
                                                 style="width: 290px ; height: 220px;"
-                                                alt="Wheaton Chicagos  sam ice cream"
+                                                alt="Wheaton-Chicagos-sam-ice-cream"
                                                 loading="lazy"
                                                 >
                                         </div>
@@ -485,7 +485,7 @@
                                     data-bs-slide="prev">
                                     <i class="fa fa-angle-left"></i>
                                 </a>
-                                <a class="carousel-control-next" data-bs-target="#main_slider" role="button"
+                                <a class="carousel-control-next" data-bs-target="#main_slider" role="button" 
                                     data-bs-slide="next">
                                     <i class="fa fa-angle-right"></i>
                                 </a>
@@ -511,7 +511,7 @@
                         <div class="map-container">
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d380511.58486109035!2d-88.06153032415246!3d41.833750983111194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c3cd0f4cbed%3A0xafe0a6ad09c0c000!2sChicago%2C%20IL%2C%20USA!5e0!3m2!1sen!2sjo!4v1741814496501!5m2!1sen!2sjo"
-                                loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Wheaton-Chicagos-sam-ice-cream">
                             </iframe>
                         </div>
                     </div>
